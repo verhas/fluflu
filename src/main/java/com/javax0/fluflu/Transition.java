54f8ae3bef892fdeba1fba062587e391ecd58ed4
Peter Verhas <peter@verhas.com>
2014-06-08 19:35:56 +0200

    added repeatable to the transition annotation and changed POM to 1.8 java

--- src/main/java/com/javax0/fluflu/Transition.java
+++ src/main/java/com/javax0/fluflu/Transition.java
@@ -1,12 +1,14 @@
 package com.javax0.fluflu;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
+@Repeatable(Transitions.class)
 public @interface Transition {
 	String[] from() default "";
 
